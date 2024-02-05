8eb02e8778f923683a4560c50695909ccc4562bf
Zack Cain <zacharylcain@gmail.com>
2024-02-05 14:19:54 -0600

    changes to scrabble

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -34,20 +34,71 @@ function oldScrabbleScorer(word) {
 
 function initialPrompt() {
    console.log("Let's play some scrabble! Enter a word:");
-   const userInput = input.question("Enter your word to score:");
+   const userInput = input.question("Enter a word to score!:");
    const score = oldScrabbleScorer(userInput);
    console.log(`Your word:${userInput}\n${score}`);
 }
 
-let simpleScorer = Object.points(word)
+function simpleScorer(word){
+let lowerCaseWord = word.toLowerCase();
+let lettersArray = lowerCaseWord.split(',');
+let wordScore = 0;
+for(i=0;i< lettersArray.length;i++){
+   wordScore = wordScore + 1;
+}
+return wordScore;
+}
 
-let vowelBonusScorer;
+function vowelBonusScorer(word){
+const vowels = ('A','E','I,','O','U')
+const wordScore = 0;
+for(let i = 0;i<word.length;i++){
+   if (vowels.includes(word[i].toUpperCase())) {
+      wordScore += 3;
+    } else {
+      wordScore += 1;
+    }
+  }
+  return wordScore;
+}
 
-let scrabbleScorer;
+function scrabbleScorer(word){
+let lowerCaseWord = word.length();
+let lettersArray = lowerCaseWord.split(',');
+const points = oldScrabbleScorer(lettersArray);
 
-const scoringAlgorithms = [];
+return points;
+}
 
-function scorerPrompt() {}
+
+const scoringAlgorithms = [
+   {
+   name: 'simpleScorer',
+   description: 'Each letter is worth one point',
+   scoringFunction: simpleScorer
+   },
+   {
+      name: 'Bonous Vowels',
+      description: 'Vowels are 3 points, Constinents are 1 point',
+      scoringFunction: vowelBonusScorer
+   },
+   {
+      name: 'Scrabble' ,
+      descritption: 'Scrabble the Traditional scoring algorythem',
+      scoringFunction: scrabbleScorer
+   }
+];
+
+function scorerPrompt() {
+   console.log("Which Scoreing Algorythem would you like to use?\n:");
+   for(i=0;i<scoringAlgorithms.length;i++){
+      console.log(`${i} - ${scoringAlgorithms[i].name}: ${scoringAlgorithms[i].description}`);
+   }
+   const selectedAlgorithmIndex = input.question('Enter 0, 1, or 2: ');
+
+  return scoringAlgorithms[selectedAlgorithmIndex];
+}
+
 
 function transform() {};
 
